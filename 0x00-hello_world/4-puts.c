b7056be4225c8f9abce8dfa0f0bec558d9709453
Ojieide Egbomondion <ojieide@yahoo.com>
2022-09-08 17:14:26 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
  * main - to start and end the program
- * void - an empty data type that has no return value
- *
- *
- *
+ * void: an empty data type that has no return value
  * Return: 0 means the program executed successfully
  */
 int main(void)
