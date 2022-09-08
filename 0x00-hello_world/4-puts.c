0566a8f3ee651dfdc379c7921bd961eea0042202
Ojieide Egbomondion <ojieide@yahoo.com>
2022-09-08 17:02:23 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to start and end the program
+ * void - an empty data type that has no return value
+ *
+ *
+ *
+ * Return: 0 means the program executed successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	getchar();
+	return 0;
+}
