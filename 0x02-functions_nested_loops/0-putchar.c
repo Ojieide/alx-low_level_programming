16dad55402409dbd83e2f3a09514a643e181a07c
Ojieide Egbomondion <ojieide@yahoo.com>
2022-09-13 14:19:43 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prinf: send formatted output to screen
+ * return: 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+<h1>0x02. C - Functions, nested loops</h1>
+<ul>
+<li><b>0-putchar.c</b> - This program prints \_putchar followed by a new line</li>
+</ul>
