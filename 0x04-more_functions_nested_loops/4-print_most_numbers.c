edef5ba1695c92e97cbed5fafad9e8675d670cc3
Ojieide Egbomondion <ojieide@yahoo.com>
2022-09-15 16:14:27 +0000

    Print 0 to 10 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * excluding 2 and 4
+ *
+ * @void: no argument is needed for calling the function
+ *
+ * Return: On success 0.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if ((n == '2') || (n == '4'))
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	putchar('\n');
+}
