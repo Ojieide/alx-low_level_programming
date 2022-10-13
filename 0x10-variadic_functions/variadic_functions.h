d2584d83f7143b440da2d6abc96b2b07fa76ab25
Ojieide Egbomondion <ojieide@yahoo.com>
2022-10-13 13:25:17 +0000

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list ptr);
+void print_string(va_list ptr);
+void print_float(va_list ptr);
+void print_int(va_list ptr);
+
+typedef struct type
+{
+	char *type;
+	void (*f)();
+} type_t;
 
 #endif
