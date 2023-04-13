87a47040c8e31a7af29954fd3b7d8023fd2c8a59
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-13 16:21:37 +0000

    Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,11 +1,12 @@
 #include "search_algos.h"
 
 /**
-  * advanced_binary_recursion - Searches recursively for a value in a sorted subarray
-  *				of integers using the Binary search algorithm.
+  * advanced_binary_recursion - Searches recursively for a value in a sorted
+  *                             subarray of integers using the Binary search
+  *                             algorithm.
   * @array: Pointer to the first element of the subrray to search in.
   * @x: Index of the start of the search in the subarray.
-  * @y: Index of the end of the search in the subarray.
+  * @y: The ending index of the [sub]array to search.
   * @value: Value to search for.
   *
   * Return: -1, If the value is not present in subarray or if subarray is NULL.
@@ -16,38 +17,30 @@ int advanced_binary_recursion(int *array, size_t x, size_t y, int value)
 	size_t a;
 
 	if (y < x)
-	{
 		return (-1);
-	}
 
 	printf("Searching in array: ");
 	for (a = x; a < y; a++)
-	{
 		printf("%d, ", array[a]);
-	}
 	printf("%d\n", array[a]);
 
 	a = x + (y - x) / 2;
 	if (array[a] == value && (a == x || array[a - 1] != value))
-	{
 		return (a);
-	}
 	if (array[a] >= value)
-	{
-		return (advanced_binary_recursion(array, x, a, value));
-	}
-	return (advanced_binary_recursion(array, a + 1, y, value));
+		return (advanced_binary_recursive(array, x, a, value));
+	return (advanced_binary_recursive(array, a + 1, y, value));
 }
 
 /**
   * advanced_binary - Searches for a value in a sorted array
-  *		of integers using the Binary search algorithm.
+  *					of integers using advanced Binary search algorithm.
   * @array: Pointer to the first element of the array to search in.
   * @size: Number of elements in array.
   * @value: Value to search for.
   *
   * Description: Returns the index of the first value in the array
-  *		(if this value appears more than once in the array).
+  *             (if this value appears more than once in the array).
   * Return: -1, If the value is not present in array or if array is NULL.
   *         Otherwise, the first index where the value is located.
   */
