9f7972ac19b1c565a61febe77466b0c75948f57b
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-14 17:17:28 +0000

    Prints all single base 10 digits starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        while (i <= 9)
-        {
-                printf("%d", i);
-                i++;
-        }
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
 	printf("\n");
-        return (0);
+	return (0);
 }
