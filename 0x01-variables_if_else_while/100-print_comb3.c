a40ddd6769b309052f2261c7b491879d5c31b65d
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-20 16:56:45 +0000

    Prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x, y, z;
+	x = 0;
+
+	while (x < 100)
+	{
+		/* Single digit */
+		y = x % 10;
+		/* Double digit */
+		z = x / 10;
+
+		if (z < y)
+		{
+			putchar(z + '0');
+			putchar(y + '0');
+			
+			if (x < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
