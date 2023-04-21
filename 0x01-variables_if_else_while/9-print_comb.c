4ae339946571b84b606a2a31d44fb5c408d211c8
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-21 23:12:47 +0000

    Prints all combinations of single-digit number separated by commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,17 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0');
-		if (i < 9)
+		putchar((i % 10) + '0');
+		if (i == 9)
 		{
-			putchar(',');
-			putchar(' ');
+			continue;
 		}
-		i++;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
