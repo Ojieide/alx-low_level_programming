a13c5861e6f2e03e21161f76980e7fc87df0045e
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-24 11:33:58 +0000

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - print _putchar to the screen
- * Return: 0 (Success)
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +20,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
 }
