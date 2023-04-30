c20b9865ca096208b7617e04765aacfdc4f2e7e3
Ojieide Egbomondion <ojieide@yahoo.com>
2023-04-30 11:03:32 +0000

    Prints the sum of the even-valued terms in the Fibonacci sequence

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,20 +2,20 @@
 
 /**
  * main - Finds and prints the sum of the even-valued terms
- * 	in the Fibonacci sequence
+ *	in the Fibonacci sequence
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	unsigned long int num1 = 0, num2 = 1, sum, even_sum;
-	
+
 	while (1)
 	{
 		sum = num1 + num2;
 		num1 = num2;
 		num2 = sum;
-		
+
 		if (sum > 4000000)
 		{
 			break;
@@ -28,5 +28,3 @@ int main(void)
 	printf("%lu\n", even_sum);
 	return (0);
 }
-
-		
