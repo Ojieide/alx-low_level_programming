6a7c68e43a23bcfa7323c3f47e9558ca680f982f
Ojieide Egbomondion <ojieide@yahoo.com>
2023-05-04 13:45:40 +0000

    Prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,26 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			if (j < size - i - 1)
+			for (j = 0; j < size; j++)
 			{
-				_putchar(' ');
+				if (j < size - i - 1)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
 			}
-			else
-			{
-				_putchar('#');
-			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
