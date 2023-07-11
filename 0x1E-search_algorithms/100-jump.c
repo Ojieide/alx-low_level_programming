ddf4a576a9ab5f249a0e2c85f9dcfb96e16966dc
Ojieide Egbomondion <ojieide@yahoo.com>
2023-07-11 07:06:03 +0000

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,7 +2,7 @@
 
 /**
  * jump_search - searches for a value in a sorted array of
- * 		integers, using the Jump search algorithm
+ *		integers, using the Jump search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
